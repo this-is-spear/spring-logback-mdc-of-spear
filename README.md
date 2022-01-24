43a15cf2390e20eac4db5c4a11ced58ccf883839
Imaspear <rjsckdd12@gmail.com>
2022-01-24 18:29:33 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# spring-logback-mdc
+ 스프링 logback mdc를 이용한 로그 관리 테스트입니다.
---
+++ src/main/java/spear/springlogbackmdc/SpringLogbackMdcApplication.java
@@ -0,0 +1,13 @@
+package spear.springlogbackmdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLogbackMdcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLogbackMdcApplication.class, args);
+    }
+
+}
